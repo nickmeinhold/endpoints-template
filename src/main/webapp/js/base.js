df2935e321efd9e4c0f4ef10b3f6183b9337d43e
Nick Meinhold <nick.meinhold@gmail.com>
2015-03-30 21:21:07 +1100

    Added angular controller that uses the google auth API to provide sign-in functionality.

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -11,129 +11,6 @@
 var enpointstemplatefrontend = enpointstemplatefrontend || {};
 
 /**
- * Client ID of the application (from the APIs Console).
- * @type {string}
- */
-enpointstemplatefrontend.CLIENT_ID =
-    '733256089952-v0gfvl7ca166pa1vldth1lh2l1s28ma2.apps.googleusercontent.com';
-
-/**
- * Scopes used by the application.
- * @type {string}
- */
-enpointstemplatefrontend.SCOPES =
-    'https://www.googleapis.com/auth/userinfo.email';
-
-/**
- * Whether or not the user is signed in.
- * @type {boolean}
- */
-enpointstemplatefrontend.signedIn = false;
-
-/**
- * Loads the application UI after the user has completed auth.
- */
-enpointstemplatefrontend.userAuthed = function() {
-  var request = gapi.client.oauth2.userinfo.get().execute(function(resp) {
-    if (!resp.code) {
-      enpointstemplatefrontend.signedIn = true;
-      document.getElementById('signinButton').innerHTML = 'Sign out';
-      document.getElementById('setDisplayName').disabled = false;
-      enpointstemplatefrontend.getProfile(); 
-    }
-  });
-};
-
-/**
- * Handles the auth flow, with the given value for immediate mode.
- * @param {boolean} mode Whether or not to use immediate mode.
- * @param {Function} callback Callback to call on completion.
- */
-enpointstemplatefrontend.signin = function(mode, callback) {
-  gapi.auth.authorize({client_id: enpointstemplatefrontend.CLIENT_ID,
-      scope: enpointstemplatefrontend.SCOPES, immediate: mode},
-      callback);
-};
-
-/**
- * Presents the user with the authorization popup.
- */
-enpointstemplatefrontend.auth = function() {
-  if (!enpointstemplatefrontend.signedIn) {
-    enpointstemplatefrontend.signin(false,
-        enpointstemplatefrontend.userAuthed);
-  } else {
-    enpointstemplatefrontend.signedIn = false;
-    document.getElementById('signinButton').innerHTML = 'Sign in';
-    document.getElementById('setDisplayName').disabled = true;
-    enpointstemplatefrontend.outputDisplayName('Not logged in.'); 
-  }
-};
-
-/**
- * Prints a message to the message log.
- * param {Object} message Message to print.
- */
-enpointstemplatefrontend.print = function(message) {
-  var element = document.createElement('div');
-  element.classList.add('row');
-  element.innerHTML = message;
-  document.getElementById('outputLog').appendChild(element);
-};
-
-/**
- * Outputs the display name in the banner.
- * param {Object} profile Profile with display name.
- */
-enpointstemplatefrontend.outputDisplayName = function(name) {
-  document.getElementById('displayName').innerHTML = name;
-};
-
-/**
- * Sets the user's display name via the API.
- * @param {string} name Display name for the profile.
- */
-enpointstemplatefrontend.setDisplayName = function(name) {
-  gapi.client.enpointstemplateapi.setDisplayName({'name': name}).execute(
-      function(resp) {
-        if (!resp.code) {
-        	enpointstemplatefrontend.outputDisplayName("Welcome "+resp.displayName);
-        } else {
-          window.alert(resp.message);
-        }
-      });
-};
-
-/**
- * Retrieves the profile for the current user via the API.
- * Also sets the display name. 
- */
-enpointstemplatefrontend.getProfile = function() {
-  gapi.client.enpointstemplateapi.getProfile().execute(
-      function(resp) {
-        if (!resp.code) {
-        	enpointstemplatefrontend.outputDisplayName("Welcome "+resp.displayName);
-        } else {
-          window.alert(resp.message);
-        }
-      });
-};
-
-/**
- * Enables the button callbacks in the UI.
- */
-enpointstemplatefrontend.enableButtons = function() {
-  
-	document.getElementById('setDisplayName').onclick = function() {
-		enpointstemplatefrontend.setDisplayName(document.getElementById('dispName').value); 
-	  }
-  
-	document.getElementById('signinButton').onclick = function() {
-		enpointstemplatefrontend.auth();
-	}
-};
-
-/**
  * Initializes the application.
  * @param {string} apiRoot Root of the API's path.
  */
@@ -143,8 +20,8 @@ enpointstemplatefrontend.init = function(apiRoot) {
   var apisToLoad;
   var callback = function() {
     if (--apisToLoad == 0) {
-      enpointstemplatefrontend.enableButtons();
-      enpointstemplatefrontend.signin(true, enpointstemplatefrontend.userAuthed);
+      angular.bootstrap(document, ['endpointstemplateAngApp']); // Bootstrap the angular module after loading the 
+		// Google libraries so the Google JavaScript library is ready in the angular modules.
     }
   }
 
@@ -152,3 +29,112 @@ enpointstemplatefrontend.init = function(apiRoot) {
   gapi.client.load('enpointstemplateapi', 'v1', callback, apiRoot);
   gapi.client.load('oauth2', 'v2', callback);
 };
+
+
+/**
+ * @ngdoc object
+ * @name endpointstemplateAngApp
+ * @requires $routeProvider
+ * @requires conferenceControllers
+ * @requires ui.bootstrap
+ *
+ * @description
+ * Root app, which routes and specifies the partial html and controller depending on the url requested.
+ * ui.bootstrap is used to show the login modal window   
+ *
+ */
+var angApp = angular.module('endpointstemplateAngApp',
+    ['endpointstemplateControllers', 'ngRoute', 'ui.bootstrap']).config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/view1', {
+                    templateUrl: '/partials/view1.html',
+                    controller: 'View1Ctrl'
+                }).
+                when('/view2', {
+                    templateUrl: '/partials/view2.html',
+                    controller: 'View2Ctrl'
+                }).
+                when('/conference/detail/:websafeConferenceKey', {
+                    templateUrl: '/partials/conference_detail.html',
+                    controller: 'ConferenceDetailCtrl'
+                }).
+                when('/profile', {
+                    templateUrl: '/partials/profile.html',
+                    controller: 'MyProfileCtrl'
+                }).
+                when('/', {
+                    templateUrl: '/partials/home.html'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
+        }]);
+
+/**
+ * @ngdoc service
+ * @name oauth2Provider
+ *
+ * @description
+ * Service that holds the OAuth2 information shared across all the pages.
+ *
+ */
+angApp.factory('oauth2Provider', function ($modal) {
+    var oauth2Provider = {
+        CLIENT_ID: '733256089952-v0gfvl7ca166pa1vldth1lh2l1s28ma2.apps.googleusercontent.com',
+        SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
+        signedIn: false
+    };
+
+    /**
+     * Calls the OAuth2 authentication method.
+     */
+    oauth2Provider.signIn = function (callback) {
+        gapi.auth.signIn({
+            'clientid': oauth2Provider.CLIENT_ID,
+            'cookiepolicy': 'single_host_origin',
+            'accesstype': 'online',
+            'approveprompt': 'auto',
+            'scope': oauth2Provider.SCOPES,
+            'callback': callback
+        });
+    };
+
+    /**
+     * Logs out the user.
+     */
+    oauth2Provider.signOut = function () {
+        gapi.auth.signOut();
+        // Explicitly set the invalid access token in order to make the API calls fail.
+        gapi.auth.setToken({access_token: ''})
+        oauth2Provider.signedIn = false;
+    };
+
+    /**
+     * Shows the modal with Google+ sign in button.
+     *
+     * @returns {*|Window}
+     */
+    oauth2Provider.showLoginModal = function() {
+        var modalInstance = $modal.open({
+            templateUrl: '/partials/login.modal.html',
+            controller: 'OAuth2LoginModalCtrl'
+        });
+        return modalInstance;
+    };
+
+    return oauth2Provider;
+});
+
+/**
+ * @ngdoc constant
+ * @name HTTP_ERRORS
+ *
+ * @description
+ * Holds the constants that represent HTTP error codes.
+ *
+ */
+angApp.constant('HTTP_ERRORS', {
+    'UNAUTHORIZED': 401
+});
+
---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,171 @@
+'use strict'; // so that using undeclared variables will cause an error 
+
+/**
+ * The root endpointstemplateAngApp module.
+ *
+ * @type {endpointstemplateAngApp|*|{}}
+ */
+var endpointstemplateAngApp = endpointstemplateAngApp || {};
+
+/**
+ * @ngdoc module
+ * @name endpointstemplateControllers
+ *
+ * @description
+ * Angular module for controllers.
+ *
+ */
+endpointstemplateAngApp.controllers = angular.module('endpointstemplateControllers', ['ui.bootstrap']);
+
+/**
+ * @ngdoc controller
+ * @name RootCtrl
+ *
+ * @description
+ * The root controller having a scope of the body element and methods used in the application wide
+ * such as user authentications.
+ *
+ */
+endpointstemplateAngApp.controllers.controller('RootCtrl', function ($scope, $location, oauth2Provider) {
+
+    /**
+     * Returns if the viewLocation is the currently viewed page.
+     *
+     * @param viewLocation
+     * @returns {boolean} true if viewLocation is the currently viewed page. Returns false otherwise.
+     */
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+    
+    /**
+     * Returns the OAuth2 signedIn state.
+     *
+     * @returns {oauth2Provider.signedIn|*} true if siendIn, false otherwise.
+     */
+    $scope.getSignedInState = function () {
+        return oauth2Provider.signedIn;
+    };
+
+    /**
+     * Calls the OAuth2 authentication method.
+     */
+    $scope.signIn = function () {
+        oauth2Provider.signIn(function () {
+            gapi.client.oauth2.userinfo.get().execute(function (resp) {
+                $scope.$apply(function () {
+                    if (resp.email) {
+                        oauth2Provider.signedIn = true;
+                        $scope.alertStatus = 'success';
+                        $scope.rootMessages = 'Logged in with ' + resp.email;
+                    }
+                });
+            });
+        });
+    };
+
+    /**
+     * Render the signInButton and restore the credential if it's stored in the cookie.
+     * (Just calling this to restore the credential from the stored cookie. So hiding the signInButton immediately
+     *  after the rendering)
+     */
+    $scope.initSignInButton = function () {
+        gapi.signin.render('signInButton', {
+            'callback': function () {
+                if (gapi.auth.getToken() && gapi.auth.getToken().access_token) {
+                    $scope.$apply(function () {
+                        oauth2Provider.signedIn = true;
+                    });
+                }
+            },
+            'clientid': oauth2Provider.CLIENT_ID,
+            'cookiepolicy': 'single_host_origin',
+            'scope': oauth2Provider.SCOPES
+        });
+    };
+
+    /**
+     * Logs out the user.
+     */
+    $scope.signOut = function () {
+        oauth2Provider.signOut();
+        $scope.alertStatus = 'success';
+        $scope.rootMessages = 'Logged out';
+    };
+
+    /**
+     * Collapses the navbar on mobile devices.
+     */
+    $scope.collapseNavbar = function () {
+        angular.element(document.querySelector('.navbar-collapse')).removeClass('in');
+    };
+
+});
+
+/**
+ * @ngdoc controller
+ * @name OAuth2LoginModalCtrl
+ *
+ * @description
+ * The controller for the modal dialog that is shown when an user needs to login to achieve some functions.
+ *
+ */
+endpointstemplateAngApp.controllers.controller('OAuth2LoginModalCtrl',
+    function ($scope, $modalInstance, $rootScope, oauth2Provider) {
+        $scope.singInViaModal = function () {
+            oauth2Provider.signIn(function () {
+                gapi.client.oauth2.userinfo.get().execute(function (resp) {
+                    $scope.$root.$apply(function () {
+                        oauth2Provider.signedIn = true;
+                        $scope.$root.alertStatus = 'success';
+                        $scope.$root.rootMessages = 'Logged in with ' + resp.email;
+                    });
+
+                    $modalInstance.close();
+                });
+            });
+        };
+    });
+
+/**
+ * @ngdoc controller
+ * @name View1Ctrl
+ *
+ * @description
+ * A controller used for the My Profile page.
+ */
+endpointstemplateAngApp.controllers.controller('View1Ctrl',
+    function ($scope, $log, oauth2Provider, HTTP_ERRORS) {
+        $scope.submitted = false;
+        $scope.loading = false;
+
+        /**
+         * The initial profile retrieved from the server to know the dirty state.
+         * @type {{}}
+         */
+        $scope.initialProfile = {};
+
+    }
+);
+
+/**
+ * @ngdoc controller
+ * @name View1Ctrl
+ *
+ * @description
+ * A controller used for the My Profile page.
+ */
+endpointstemplateAngApp.controllers.controller('View2Ctrl',
+    function ($scope, $log, oauth2Provider, HTTP_ERRORS) {
+        $scope.submitted = false;
+        $scope.loading = false;
+
+        /**
+         * The initial profile retrieved from the server to know the dirty state.
+         * @type {{}}
+         */
+        $scope.initialProfile = {};
+
+    }
+);
+
